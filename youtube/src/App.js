4695411ad0267df45ac72fc5296ac98a8f0a4891
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-15 12:03:43 +0000

    Initialized our YouTube app

---
+++ youtube/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+          Buy now
+        </button>
+        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+          Add to bag
+        </button>
+    </div>
+  );
+}
+
+export default App;
