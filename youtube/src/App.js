497d0985a3cde1be7e6fc2daa709d194e9132dc9
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 20:20:43 +0000

    Added store to our App.js via Provider component

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -15,13 +15,17 @@ App Layout:
 import './App.css';
 import Head from './components/Head';
 import Body from './components/Body';
+import { Provider } from 'react-redux';
+import { store } from './utils/store';
 
 function App() {
   return (
+    <Provider store = {store}>
     <div>
       <Head />
       <Body />
     </div>
+    </Provider>
   );
 }
 
