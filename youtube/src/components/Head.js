b382630a3d8957c46f8d7c90c50d6bec5c0782f8
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 18:00:32 +0000

    Created a Head Navbar for Youtube app

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -2,24 +2,29 @@ import React from "react";
 
 const Head = () => {
   return (
-    <div>
-      <div>
+    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
+      <div className="flex col-span-1">
         <img
+          className="h-5 my-0.5"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKi1yF1PLdUkait_4ixaBVkz3jeyfD_-yb-g&usqp=CAU"
           alt="hamburger-img"
         />
         <img
+          className="h-6 mx-4"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png?20220605194644"
           alt="YouTube_logo"
         />
       </div>
-      <div>
+      <div className="col-span-10">
         <input type="text" />
         <button>Search</button>
-
       </div>
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-Gh6uC11b9BUzfJ1OAuC3MgwwQdOLZL7PA&usqp=CAU" alt="user-icon" />
+      <div className="col-span-1">
+        <img
+        className="h-8"
+          src="https://www.seekpng.com/png/small/41-410093_circled-user-icon-user-profile-icon-png.png"
+          alt="user-icon"
+        />
       </div>
     </div>
   );
