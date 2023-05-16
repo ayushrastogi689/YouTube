14364773ec13d132df47304e62de5bc9a5cfca80
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 17:23:28 +0000

    Added Icons in our Head component: 1. YouTube Logo 2. Hamgurger Menu 3. User Icon

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
     <div>
-      <h1>Head</h1>
-    </div>
-  )
-}
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKi1yF1PLdUkait_4ixaBVkz3jeyfD_-yb-g&usqp=CAU"
+          alt="hamburger-img"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png?20220605194644"
+          alt="YouTube_logo"
+        />
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
 
-export default Head
+      </div>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-Gh6uC11b9BUzfJ1OAuC3MgwwQdOLZL7PA&usqp=CAU" alt="user-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
