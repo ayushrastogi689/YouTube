1bec50ec5c15875cdeba1debf54c0bbecfdda69f
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 21:41:20 +0000

    Added dispatch and toggleMenuHandler function

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { useDispatch } from "react-redux"; // react-redux is a library
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  // dispatch will come from our useDispatch() hook, useDispatch() hook comes from our react-redux library
+  const dispatch = useDispatch(); //
+
+  // This a function which is being called in onClick and this will dispatch an action
+  const toggleMenuHandler = () => {
+  // Now we will dispatch an action by calling the const dispatch function by passing toggle menu inside it
+    dispatch(toggleMenu())
+  }; 
+
+
+
   return (
     <div className="grid grid-flow-col p-4 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-6 my-0.5"
+          className="h-6 my-0.5 cursor-pointer"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKi1yF1PLdUkait_4ixaBVkz3jeyfD_-yb-g&usqp=CAU"
           alt="hamburger-img"
+          onClick={() => toggleMenuHandler()}
         />
+        <a href="/">
         <img
           className="h-7 mx-4"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png?20220605194644"
           alt="YouTube_logo"
         />
+        </a>
       </div>
       <div className="col-span-10">
         <input className = "w-1/2 p-1 border border-gray-400 rounded-l-full" type="text" />
