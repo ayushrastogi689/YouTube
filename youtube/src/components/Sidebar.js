9cbd666a63b5aeccdcf96e76c73d4309b2fe846a
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 19:25:26 +0000

    Feature - Created Sidebar Content

--- youtube/src/components/Sidebar.js
+++ youtube/src/components/Sidebar.js
@@ -3,8 +3,15 @@ import MenuItems from './MenuItems'
 
 const Sidebar = () => {
   return (
-    <div>
-        Sidebar
+    <div className='p-5 shadow-lg w-48'>
+        <h1 className='font-bold'>Subscriptions</h1>
+        <ul>
+          <li>Music</li>
+          <li>Movies</li>
+          <li>News</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+        </ul>
         <MenuItems />
     </div>
   )
