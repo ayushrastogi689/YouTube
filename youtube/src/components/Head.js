129440f75f47bdd30ce3faaa4e4981499a579253
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-16 19:02:04 +0000

    CSS - Added styling to our search bar

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -5,19 +5,19 @@ const Head = () => {
     <div className="grid grid-flow-col p-4 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-5 my-0.5"
+          className="h-6 my-0.5"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKi1yF1PLdUkait_4ixaBVkz3jeyfD_-yb-g&usqp=CAU"
           alt="hamburger-img"
         />
         <img
-          className="h-6 mx-4"
+          className="h-7 mx-4"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png?20220605194644"
           alt="YouTube_logo"
         />
       </div>
       <div className="col-span-10">
-        <input type="text" />
-        <button>Search</button>
+        <input className = "w-1/2 p-1 border border-gray-400 rounded-l-full" type="text" />
+        <button className="p-1 bg-gray-100 border border-gray-400 rounded-r-full">Search</button>
       </div>
       <div className="col-span-1">
         <img
