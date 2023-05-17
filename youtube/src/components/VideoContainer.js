98862bf7361502eaa98d8783d2e6474fb206086b
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 07:33:48 +0000

    Fetch YouTube Video data from live api of YouTube

--- youtube/src/components/VideoContainer.js
+++ youtube/src/components/VideoContainer.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VideoCard from "./VideoCard"
+import { YOUTUBE_VIDEO_API } from '../utils/constant';
+
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async() => {
+    const responseData = await fetch(YOUTUBE_VIDEO_API);
+    const jsonData = await responseData.json();
+    console.log(jsonData);
+    setVideos(jsonData.items)
+  }
   return (
     <div>
       VideoContainer
