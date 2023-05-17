922d4ac485d8d7f6db9ff0b321431d4acee1e3a6
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 07:09:02 +0000

    Feature - Made Sidebar show & collapse functional

--- youtube/src/components/Sidebar.js
+++ youtube/src/components/Sidebar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import MenuItems from './MenuItems'
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
+  // In our sidebar we need to show and hide the sidebar on click of hamburger menu. To achive this we will use useSelector
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen); // You neet to subscribe to a specific portion of your store.
+  if(!isMenuOpen) return null; // Early return pattern
+
   return (
     <div className='p-5 shadow-lg w-48'>
+      <ul>
+        <li>Home</li>
+        <li>Videos</li>
+        <li>Shorts</li>
+        <li>Live</li>
+      </ul>
         <h1 className='font-bold'>Subscriptions</h1>
+        <ul>
+          <li>Music</li>
+          <li>Movies</li>
+          <li>News</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+        </ul>
+        <h1 className='font-bold'>Watch Later</h1>
         <ul>
           <li>Music</li>
           <li>Movies</li>
