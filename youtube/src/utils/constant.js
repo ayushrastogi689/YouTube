6ec7d21942df08df5347ac22efcf82703ba360ad
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 11:23:33 +0000

    Added constant for keeping const variables in utils

---
+++ youtube/src/utils/constant.js
@@ -0,0 +1,4 @@
+const YOUTUBE_API_KEY = "AIzaSyBPQp3pH3-7escPwdetMoiawXMQzHqt1OU";
+
+export const YOUTUBE_VIDEO_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key='+YOUTUBE_API_KEY ;
+
