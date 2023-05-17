412f76e9255ea16310ed3508f081c2c1d64e0e4d
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 18:26:16 +0000

    Feature - Created a dispach action in Watch page so that Sidebar collapse automatically when in Vido Watch Page

--- youtube/src/components/WatchPage.js
+++ youtube/src/components/WatchPage.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {useEffect} from "react"
+import {closeMenu} from "../utils/appSlice"
 
 const WatchPage = () => {
+  // When ever we try to watch any video our Sidebar should collapse automatically.
+  // Q. How we can do that? => We have to dispatch an action.  
+  // Q. Where we will do that? => Inside our WatchPage in useEffect
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
   return (
     <div>
       Watch Videos Here
