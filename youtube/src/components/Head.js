8e902e1f15955a840b5c32dbbca22375b7c4ba37
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 07:06:47 +0000

    CSS - Updated the styling of head

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -15,29 +15,29 @@ const Head = () => {
 
 
   return (
-    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
+    <div className="grid grid-flow-col p-3 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-6 my-0.5 cursor-pointer"
+          className="h-5 my-0.5 cursor-pointer"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKi1yF1PLdUkait_4ixaBVkz3jeyfD_-yb-g&usqp=CAU"
           alt="hamburger-img"
-          onClick={() => toggleMenuHandler()}
+          onClick={() => toggleMenuHandler()} // Dispatch and action
         />
         <a href="/">
         <img
-          className="h-7 mx-4"
+          className="h-6 mx-4"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png?20220605194644"
           alt="YouTube_logo"
         />
         </a>
       </div>
-      <div className="col-span-10">
-        <input className = "w-1/2 p-1 border border-gray-400 rounded-l-full" type="text" />
-        <button className="p-1 bg-gray-100 border border-gray-400 rounded-r-full">Search</button>
+      <div className="col-span-10 mb-1">
+        <input className = "w-1/2 p-0.5 border border-gray-400 rounded-l-full" type="text" />
+        <button className="p-0.5 bg-gray-100 border border-gray-400 rounded-r-full">Search</button>
       </div>
       <div className="col-span-1">
         <img
-        className="h-8"
+        className="h-6"
           src="https://www.seekpng.com/png/small/41-410093_circled-user-icon-user-profile-icon-png.png"
           alt="user-icon"
         />
