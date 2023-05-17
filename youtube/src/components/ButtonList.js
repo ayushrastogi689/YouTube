a93f1d8650c32dca90d77ded72d9dc0fa6db8061
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 11:20:57 +0000

    Updated ButtonList with a New AI button

--- youtube/src/components/ButtonList.js
+++ youtube/src/components/ButtonList.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Button from './Button'
+
+const listOfButtons = ["All", "Live", "News", "Songs", "Movies", "Comedy", "Adventures", "Gaming", "Tech", "AI", "JavaScript", "React"];
 
 const ButtonList = () => {
   return (
-    <div>
-      ButtonList
+    <div className='flex'>
+      <Button name="All"/>
+      <Button name="Live"/>
+      <Button name="News"/>
+      <Button name="Songs"/>
+      <Button name="Movies"/>
+      <Button name="Comedy"/>
+      <Button name="Adventures"/>
+      <Button name="Gaming"/>
+      <Button name="AI"/>
+      <Button name="Tech"/>
+      <Button name="JavaScript"/>
+      <Button name="React"/>
+      <Button name="Node.js"/>
     </div>
   )
 }
