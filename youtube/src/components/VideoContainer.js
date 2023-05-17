426baf8a7bd61b6e10b0cd9cc765edd23cce9e3f
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 19:31:15 +0000

    Warning Fix - Added key={} to the Link component isntead of VideoCard component

--- youtube/src/components/VideoContainer.js
+++ youtube/src/components/VideoContainer.js
@@ -21,8 +21,8 @@ const VideoContainer = () => {
     {
       videos.length > 0 &&
         videos.map((video) => (
-         <Link to={"/watch?v="+video.id}> 
-          <VideoCard key={video.id} info={video} />
+         <Link to={"/watch?v="+video.id} key={video.id}> 
+          <VideoCard info={video} />
          </Link>
         )
       )
