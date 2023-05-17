ebab936f9236c6dcbb18dcd88d4f7677a762b4ab
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-17 10:12:27 +0000

    Created a Video Card component for VideoContainer

--- youtube/src/components/VideoCard.js
+++ youtube/src/components/VideoCard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const VideCard = () => {
+const VideoCard = ({info}) => {
+  const {snippet, statistics} = info;
+  const { title, channelTitle, thumbnails } = snippet;
+
+  console.log(info);
+
   return (
-    <div>
-      VideoCard
+    <div className='p-2 m-2 w-72 shadow-lg'>
+       <img src={thumbnails.medium.url} alt="thumbnail" />
+      <ul>
+        <li className='font-bold'>{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} views</li>
+      </ul>
     </div>
   )
 }
 
-export default VideCard
+export default VideoCard
