1c430a1b486b47d0859a108f4bc7d902e0aeb6a6
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-05-19 15:07:39 +0000

    Feature - Created a watch video feature using useSearchParams

--- youtube/src/components/WatchPage.js
+++ youtube/src/components/WatchPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import {useEffect} from "react"
 import {closeMenu} from "../utils/appSlice"
-
+import {useSearchParams} from "react-router-dom"
 const WatchPage = () => {
   // When ever we try to watch any video our Sidebar should collapse automatically.
   // Q. How we can do that? => We have to dispatch an action.  
@@ -13,9 +13,14 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
 
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"))
   return (
     <div>
-      Watch Videos Here
+      <iframe width="960" height="515" title="YouTube video player"
+        src={"https://www.youtube.com/embed/"+searchParams.get("v")}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen>
+      </iframe>
     </div>
   )
 }
